b9d988a1ee45a43a5964a68d2dc57cc68709f2eb
DenisValuev <valuev.denis91@mail.ru>
2024-09-04 16:37:11 +0300

    Добавьте файлы проекта.

---
+++ Module9.Unit1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module9.Unit1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
