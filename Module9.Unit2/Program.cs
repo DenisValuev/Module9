11c3dcfff25c01fbeb4e2c063a91d71277b70bbe
DenisValuev <valuev.denis91@mail.ru>
2024-09-04 17:21:05 +0300

    Module9-master: Add new Unit2

---
+++ Module9.Unit2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Module9.Unit2
+{
+    internal class Program
+    {
+        static int Division(int a, int b)
+        {
+            return a / b;
+        }
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    int result = Division(7, 0);
+            //    Console.WriteLine(result);
+            //}
+            //catch (Exception e)
+            //{
+            //    if (e is DivideByZeroException)
+            //    {
+            //        Console.WriteLine("На ноль делить нельзя!");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Произошла непредвиденная ошибка в приложении.");
+            //    }
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Блок Finally сработал!");
+            //}
+            try
+            {
+
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (RankException e)
+            { 
+                Console.WriteLine(e.GetType);
+            }
+        }
+    }
+}
