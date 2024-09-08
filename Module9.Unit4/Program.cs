5a481a035246c7a34e2e97d2f361b8147523f447
DenisValuev <valuev.denis91@mail.ru>
2024-09-08 18:31:11 +0300

    Module9-master: add new Unit4

---
+++ Module9.Unit4/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Module9.Unit4
+{
+    class Car 
+    {
+        public string Model { get; set; }
+    }
+    class Lexus : Car { }
+    class Parent 
+    {
+        public string Name { get; set; }
+    }
+    class Child : Parent { }
+    internal class Program
+    {
+        public delegate Car CarDelegate(string name);
+        public delegate void ChildInfo(Child child);
+        static void Main(string[] args)
+        {
+            CarDelegate carDelegate;
+            carDelegate = BuildLexus;
+            Car c = carDelegate("GX460");
+            Console.WriteLine(c.Model);
+
+            ChildInfo childInfo = GetParentInfo;
+            Child child = new Child
+            {
+                Name = "Nikita"
+            };
+            childInfo(child);
+
+        }
+        static Lexus BuildLexus(string model)
+        {
+            return new Lexus { Model = model };
+        }
+        static void GetParentInfo( Parent p)
+        {
+            Console.WriteLine(p.Name);
+        }
+    }
+}
