a6bc3277a3a73ac0d2516706d19056407a990ad7
DenisValuev <valuev.denis91@mail.ru>
2024-09-09 12:03:07 +0300

    Module9-master: add new Unit5.1

---
+++ Module9.Unit5.1/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Module9.Unit5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberReader numberReader = new NumberReader();
+            numberReader.NumberEnteredEvent += ShowNumber;
+            while (true)
+            {
+                try
+                {
+                    numberReader.Read();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введены некорректные значение"); ;
+                }
+            }
+        }
+
+        static void ShowNumber(int number)
+        {
+            switch (number)
+            {
+                case 1: Console.WriteLine("Введено значение: 1"); break;
+                case 2: Console.WriteLine("Введено значение: 2"); break;
+            }
+        }
+    }
+    class NumberReader
+    {
+        public delegate void NumberReaderDelegate(int value);
+        public event NumberReaderDelegate NumberEnteredEvent;
+
+        public void Read()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Необходимо ввести значение: либо 1, либо 2");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number != 1 && number != 2) throw new FormatException();
+
+            NumberEntered(number);
+        }
+
+        protected virtual void NumberEntered(int number)
+        {
+            NumberEnteredEvent.Invoke(number);
+        }
+    }
+}
