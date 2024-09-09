b683c1735cad9d51f768c61d01dda50734a12941
DenisValuev <valuev.denis91@mail.ru>
2024-09-09 11:17:47 +0300

    Module9-master: add new Unit5

---
+++ Module9.Unit5/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Module9.Unit5
+{
+    public delegate void Notify();//делегат
+    public class ProcessBusinessLogic
+    {
+        public event Notify ProcessCompleted;//событие
+
+        public void StartProcess()
+        {
+            Console.WriteLine("Процесс начат!");
+            OnProcessCompleted();
+        }
+        protected virtual void OnProcessCompleted()
+        {
+            ProcessCompleted?.Invoke();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessBusinessLogic bl = new ProcessBusinessLogic();
+            bl.ProcessCompleted += bl_ProcessCompleted;// регистрируем событие
+            bl.StartProcess();
+        }
+        public static void bl_ProcessCompleted()
+        {
+            Console.WriteLine("Процесс завершен!");
+        }
+    }
+}
